8a017c4afcfc6d04a1b89c9f3fd61a48d21aa60f
Don Cross <cosinekitty@gmail.com>
2023-09-23 23:33:07 -0400

    Sloth: added knob and output ports.

--- src/SlothTorpor.cpp
+++ src/SlothTorpor.cpp
@@ -80,6 +80,13 @@ struct SlothTorporWidget : ModuleWidget
     {
         setModule(module);
         setPanel(createPanel(asset::plugin(pluginInstance, "res/SlothTorpor.svg")));
+
+        const float knobHeight = 18.3f;
+        addParam(createParam<Davies1900hBlackKnob>(mm2px(Vec(3.8f, knobHeight)), module, SlothTorporModule::CHAOS_KNOB_PARAM));
+
+        const float outputHeight = 95.0f;
+        addOutput(createOutput<PJ301MPort>(mm2px(Vec(1.5f,  outputHeight)), module, SlothTorporModule::Y_OUTPUT));
+        addOutput(createOutput<PJ301MPort>(mm2px(Vec(10.5f, outputHeight)), module, SlothTorporModule::X_OUTPUT));
     }
 };
 
