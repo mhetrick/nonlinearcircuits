b1da4168d61376dc2fdacc5f0aa20e3a91e9de58
Don Cross <cosinekitty@gmail.com>
2023-09-26 17:01:17 -0400

    Refactored Sloth Torpor module/widget classes to work for Apathy and Inertia.

--- src/Sloth.cpp
+++ src/Sloth.cpp
@@ -1,7 +1,7 @@
 /*
-    SlothTorpor.cpp  -  Don Cross <cosinekitty@gmail.com>  -  2023-09-23
+    Sloth.cpp  -  Don Cross <cosinekitty@gmail.com>  -  2023-09-23
 
-    A VCV Rack version of the Sloth Chaos "Torpor" hardware module by Andrew Fitch:
+    A VCV Rack version of the Sloth hardware modules by Andrew Fitch:
     https://www.nonlinearcircuits.com/modules/p/4hp-sloth-chaos
 */
 
@@ -9,10 +9,8 @@
 #include "SlothCircuit.hpp"
 
 
-struct SlothTorporModule : Module
+namespace SlothTypes
 {
-    Analog::TorporSlothCircuit circuit;
-
     enum ParamIds
     {
         BALANCE_KNOB_PARAM,
@@ -37,9 +35,18 @@ struct SlothTorporModule : Module
         INDICATOR_LIGHT_GREEN,
         LIGHTS_LEN
     };
+}
 
-    SlothTorporModule()
+
+template <typename circuit_t>
+struct SlothModule : Module
+{
+    circuit_t circuit;
+
+    SlothModule()
     {
+        using namespace SlothTypes;
+
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 
         configOutput(X_OUTPUT, "Small");
@@ -67,6 +74,8 @@ struct SlothTorporModule : Module
 
     void process(const ProcessArgs& args) override
     {
+        using namespace SlothTypes;
+
         circuit.setKnobPosition(params[BALANCE_KNOB_PARAM].getValue());
         circuit.update(args.sampleRate);
         outputs[X_OUTPUT].setVoltage(circuit.xVoltage(), 0);
@@ -86,29 +95,42 @@ struct RedGreenLightWidget : GrayModuleLightWidget {
     }
 };
 
-struct SlothTorporWidget : ModuleWidget
+template <typename circuit_t>
+struct SlothWidget : ModuleWidget
 {
     RedGreenLightWidget *indicatorLight{};
 
-    explicit SlothTorporWidget(SlothTorporModule* module)
+    SlothWidget(SlothModule<circuit_t>* module, const char *panelSvgFileName)
     {
+        using namespace SlothTypes;
+
         setModule(module);
-        setPanel(createPanel(asset::plugin(pluginInstance, "res/SlothTorpor.svg")));
+        setPanel(createPanel(asset::plugin(pluginInstance, panelSvgFileName)));
 
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
         addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
         const float knobHeight = 18.3f;
-        addParam(createParam<Davies1900hWhiteKnob>(mm2px(Vec(3.8f, knobHeight)), module, SlothTorporModule::BALANCE_KNOB_PARAM));
+        addParam(createParam<Davies1900hWhiteKnob>(mm2px(Vec(3.8f, knobHeight)), module, BALANCE_KNOB_PARAM));
 
         const float outputHeight = 95.0f;
-        addOutput(createOutput<PJ301MPort>(mm2px(Vec(1.5f,  outputHeight)), module, SlothTorporModule::Y_OUTPUT));
-        addOutput(createOutput<PJ301MPort>(mm2px(Vec(10.5f, outputHeight)), module, SlothTorporModule::X_OUTPUT));
+        addOutput(createOutput<PJ301MPort>(mm2px(Vec(1.5f,  outputHeight)), module, Y_OUTPUT));
+        addOutput(createOutput<PJ301MPort>(mm2px(Vec(10.5f, outputHeight)), module, X_OUTPUT));
 
-        indicatorLight = createLightCentered<LargeLight<RedGreenLightWidget>>(mm2px(Vec(10.2f, 55.0f)), module, SlothTorporModule::INDICATOR_LIGHT_RED);
+        indicatorLight = createLightCentered<LargeLight<RedGreenLightWidget>>(mm2px(Vec(10.2f, 55.0f)), module, INDICATOR_LIGHT_RED);
         addChild(indicatorLight);
     }
 };
 
+
+using SlothTorporModule = SlothModule<Analog::TorporSlothCircuit>;
+
+struct SlothTorporWidget : SlothWidget<Analog::TorporSlothCircuit>
+{
+public:
+    explicit SlothTorporWidget(SlothTorporModule* module)
+        : SlothWidget<Analog::TorporSlothCircuit>(module, "res/SlothTorpor.svg")
+        {}
+};
 
 Model* modelSlothTorpor = createModel<SlothTorporModule, SlothTorporWidget>("SlothTorpor");
