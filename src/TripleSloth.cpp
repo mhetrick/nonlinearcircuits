a4c66bd0bff976762179d9da320df250816b52af
Don Cross <cosinekitty@gmail.com>
2023-09-27 13:13:30 -0400

    Added screws to Triple Sloth panel.

--- src/TripleSloth.cpp
+++ src/TripleSloth.cpp
@@ -89,6 +89,11 @@ struct TripleSlothWidget : ModuleWidget
 
         setModule(module);
         setPanel(createPanel(asset::plugin(pluginInstance, "res/TripleSloth.svg")));
+
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
     }
 };
 
